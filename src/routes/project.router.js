e584cadb8d370cc235b5245128db8eed8cd3b02e
furahaderick <yvesderricks@gmail.com>
2025-08-16 23:00:01 +0200

    Mount routes

--- src/routes/project.router.js
+++ src/routes/project.router.js
@@ -1,19 +1,47 @@
 import express from "express";
 
-import { createProject } from "../controllers/project.controller.js";
+import {
+	createProject,
+	getAllProjects,
+	getProjectDetails,
+} from "../controllers/project.controller.js";
 
-import { validateProjectCreation } from "../middleware/validator.middleware.js";
+import { bidOnProject } from "../controllers/bid.controller.js";
+import {
+	markProjectMilestone,
+	getProjectMilestones,
+} from "../controllers/milestone.controller.js";
+
+import {
+	validateProjectCreation,
+	validateBid,
+	validateMilestone,
+} from "../middleware/validator.middleware.js";
+
 import { authenticate } from "../middleware/authenticate.middleware.js";
 import { authorize } from "../middleware/authorize.middleware.js";
 
 const projectRouter = express.Router();
 
-projectRouter.post(
-	"/",
-	authenticate,
-	authorize(["home-owner"]),
-	validateProjectCreation,
-	createProject
-);
+projectRouter
+	.route("/")
+	.post(
+		authenticate,
+		authorize(["home-owner"]),
+		validateProjectCreation,
+		createProject
+	)
+	.get(authenticate, getAllProjects);
+
+projectRouter.route("/:projectId").get(authenticate, getProjectDetails);
+
+projectRouter
+	.route("/:projectId/bids")
+	.post(authenticate, authorize(["contractor"]), validateBid, bidOnProject);
+
+projectRouter
+	.route("/:projectId/milestones")
+	.post(authenticate, validateMilestone, markProjectMilestone)
+	.get(authenticate, getProjectMilestones);
 
 export default projectRouter;
